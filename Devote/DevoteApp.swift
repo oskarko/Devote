f24ff803de294f3faa784bce49883b4bb45f81db
Óscar R. Garrucho <oscar.garrucho@gmail.com>
2021-07-09 16:24:52 +0200

    Initial commit

---
+++ Devote/DevoteApp.swift
@@ -0,0 +1,22 @@
+//
+//  DevoteApp.swift
+//  Devote
+//
+//  Created by Oscar R. Garrucho.
+//  Linkedin: https://www.linkedin.com/in/oscar-garrucho/
+//  Copyright © 2021 Oscar R. Garrucho. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct DevoteApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
