ea0f8e6f5d7da9dba6960ee8218dc46c5e9b89fc
Óscar R. Garrucho <oscar.garrucho@gmail.com>
2021-07-10 16:00:34 +0200

    Create a new entry form for the task item

---
+++ Devote/View/BlankView.swift
@@ -0,0 +1,28 @@
+//
+//  BlankView.swift
+//  Devote
+//
+//  Created by Oscar R. Garrucho.
+//  Linkedin: https://www.linkedin.com/in/oscar-garrucho/
+//  Copyright © 2021 Oscar R. Garrucho. All rights reserved.
+//
+
+import SwiftUI
+
+struct BlankView: View {
+    var body: some View {
+        VStack {
+            Spacer()
+        }
+        .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .center)
+        .background(Color.black)
+        .opacity(0.5)
+        .edgesIgnoringSafeArea(.all)
+    }
+}
+
+struct BlankView_Previews: PreviewProvider {
+    static var previews: some View {
+        BlankView()
+    }
+}
