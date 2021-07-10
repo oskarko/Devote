e907e62a376fd7fbd35965bf273b01a0ad515502
Óscar R. Garrucho <oscar.garrucho@gmail.com>
2021-07-10 17:25:24 +0200

    Add custom visual blur effect

--- Devote/View/ContentView.swift
+++ Devote/View/ContentView.swift
@@ -116,15 +116,22 @@ struct ContentView: View {
                     .padding(.vertical, 0)
                     .frame(maxWidth: 640)
                 } // VStack
+                .blur(radius: showNewTaskItem ? 8 : 0, opaque: false)
+                .transition(.move(edge: .bottom))
+                .animation(.easeOut(duration: 0.5))
+                
                 // MARK: - New Task Items
                 
                 if showNewTaskItem {
-                    BlankView()
-                        .onTapGesture {
-                            withAnimation() {
-                                showNewTaskItem = false
-                            }
+                    BlankView(
+                        backgroundColor: isDarkMode ? Color.black : Color.gray,
+                        backgroundOpacity: isDarkMode ? 0.3 : 0.5
+                    )
+                    .onTapGesture {
+                        withAnimation() {
+                            showNewTaskItem = false
                         }
+                    }
                     NewTaskItemView(isShowing: $showNewTaskItem)
                 }
                 
@@ -136,6 +143,7 @@ struct ContentView: View {
             .navigationBarHidden(true)
             .background(
                 BackgroundImageView()
+                    .blur(radius: showNewTaskItem ? 8 : 0, opaque: false)
             )
             .background(
                 backgroundGradient.ignoresSafeArea(.all)
