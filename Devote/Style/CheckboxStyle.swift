e1341c597407bc21d4be58c12ee7fdcd67b679c9
Óscar R. Garrucho <oscar.garrucho@gmail.com>
2021-07-10 17:14:12 +0200

    Create a checkbox style

---
+++ Devote/Style/CheckboxStyle.swift
@@ -0,0 +1,34 @@
+//
+//  CheckboxStyle.swift
+//  Devote
+//
+//  Created by Oscar R. Garrucho.
+//  Linkedin: https://www.linkedin.com/in/oscar-garrucho/
+//  Copyright © 2021 Oscar R. Garrucho. All rights reserved.
+//
+
+import SwiftUI
+
+struct CheckboxStyle: ToggleStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        return HStack {
+            Image(systemName: configuration.isOn ? "checkmark.circle.fill" : "circle")
+                .foregroundColor(configuration.isOn ? .pink : .primary)
+                .font(.system(size: 30, weight: .semibold, design: .rounded))
+                .onTapGesture {
+                    configuration.isOn.toggle()
+                }
+            
+            configuration.label
+        } // HStack
+    }
+}
+
+struct CheckboxStyle_Previews: PreviewProvider {
+    static var previews: some View {
+        Toggle("Placeholder label", isOn: .constant(false))
+            .toggleStyle(CheckboxStyle())
+            .padding()
+            .previewLayout(.sizeThatFits)
+    }
+}
---
+++ Devote/View/ListRowItemView.swift
@@ -0,0 +1,33 @@
+//
+//  ListRowItemView.swift
+//  Devote
+//
+//  Created by Oscar R. Garrucho.
+//  Linkedin: https://www.linkedin.com/in/oscar-garrucho/
+//  Copyright © 2021 Oscar R. Garrucho. All rights reserved.
+//
+
+import SwiftUI
+
+struct ListRowItemView: View {
+    @Environment(\.managedObjectContext) var viewContext
+    @ObservedObject var item: Item
+    
+    
+    var body: some View {
+        Toggle(isOn: $item.completion) {
+            Text(item.task ?? "")
+                .font(.system(.title2, design: .rounded))
+                .fontWeight(.heavy)
+                .foregroundColor(item.completion ? Color.pink : Color.primary)
+                .padding(.vertical, 12)
+                .animation(.default)
+        } // Toogle
+        .toggleStyle(CheckboxStyle())
+        .onReceive(item.objectWillChange, perform: { _ in
+            if self.viewContext.hasChanges {
+                try? self.viewContext.save()
+            }
+        })
+    }
+}
